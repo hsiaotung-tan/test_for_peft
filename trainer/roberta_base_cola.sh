c00b04f4735c7ae8036cdb6f11833aac7c3b6311
cver4090 <reyes.tan@foxmail.com>
2024-07-14 16:58:24 +0800

    refine codes

--- trainer/roberta_base_cola.sh
+++ trainer/roberta_base_cola.sh
@@ -11,14 +11,16 @@ run_glue.py \
 --do_predict \
 --max_seq_length 512 \
 --per_device_train_batch_size 64 \
---learning_rate 3e-4 \
+--learning_rate 1e-2 \
+--lr_scheduler_type linear \
+--warmup_ratio 0.06 \
 --num_train_epochs 80 \
 --output_dir $output_dir/model \
---logging_steps 10 \
+--logging_steps 500 \
 --logging_dir $output_dir/log \
 --eval_strategy epoch \
---save_strategy epoch \
---warmup_ratio 0.06 \
+--save_strategy steps \
+--save_steps 0.25 \
 --seed 0 \
 --weight_decay 0.1 \
 --rank 1024
